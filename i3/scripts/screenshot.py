5f37126675b3dda915affedf12214bd192b44b7a
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-01-12 19:27:10 -0500

    Update UI and type of screenshot gui

--- i3/scripts/screenshot.py
+++ i3/scripts/screenshot.py
@@ -1,6 +1,9 @@
 import customtkinter as ctk
 import os
+
+ctk.set_appearance_mode("dark")
 r = ctk.CTk()
+r.attributes('-type', 'dialog')
 l = ctk.CTkEntry(r, placeholder_text="filename without .png")
 l.focus()
 l.pack()
@@ -11,7 +14,7 @@ def quit(*_):
     if tmp:
         os.system(f"maim -s ~/Pictures/Screenshots/{tmp}.png")
         print(f"Ran: maim -s ~/Pictures/Screenshots/{tmp}.png")
-ctk.CTkButton(r, command=quit, text="Enter").pack()
+ctk.CTkButton(r, command=quit, text="Enter").pack(pady=5)
 r.bind("<Return>", quit)
 r.title("Screenshot to File")
 r.mainloop()
