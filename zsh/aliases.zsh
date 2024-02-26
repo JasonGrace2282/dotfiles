4f4b5a89309420a3db5b54771a02672703c80ae3
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-26 10:59:08 -0500

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,17 +2,15 @@
 alias ll='ls -a --color=auto'
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
+alias grepi='grep -i --color=auto'
 alias nmcli='nmcli --color yes -p'
 alias cat='bat -pp'
 
 # lazyness of typing
-alias shell='poetry shell'
-alias pi='poetry install'
 alias ff="firefox"
 alias cd='z'
 alias l='deactivate'
 alias :q='exit'
-alias html='make html'
 alias copy='xclip -sel clip'
 alias pkglist='bat /etc/pkglist.txt' # from hook
 alias pkgcount='wc -l /etc/pkglist.txt'
@@ -27,6 +25,9 @@ alias icat='kitten icat'
 alias d='kitten diff'
 
 alias py='python '
+
+# tmux
 alias mx='tmux'
 alias mxa='tmux attach'
 alias mxat='tmux attach -t'
+alias mxls='tmux ls'
