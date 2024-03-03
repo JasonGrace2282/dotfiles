3c46a50bf1dcbb4c955545236280156a5ff848ae
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-03-03 09:59:37 -0500

    Update env vars based off XDG_SESSION_TYPE

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,17 +2,16 @@ export KITTY_SHELL_INTEGRATION="enabled"
 export PATH="$HOME/.ghcup/bin:$PATH"
 export AUTOENV_FILE_LEAVE=".autoenv.zsh"
 export BAT_THEME="TwoDark"
-export CHROOT="/home/jasongrace2282/chroot"
+export CHROOT="$HOME/chroot"
 export ZSH_AUTOSUGGEST_STRATEGY=history
-export MOZ_ENABLE_WAYLAND=1
 export EDITOR='nvim'
 
 export SCREENSHOTS="~/Pictures/Screenshots"
-export REPOS="~/repos"
-export SCHOOL="~/repos/school"
-export DOCUMENTS="~/Documents"
-export DOWNLOADS="~/Downloads"
-export QTILELOG="~/.local/share/qtile/qtile.log"
-
 export DIFFPROG=nvim
+
+if [[ "${XDG_SESSION_TYPE}" == "wayland" ]]; then
+  export MOZ_ENABLE_WAYLAND=1
+else
+  export MOZ_ENABLE_WAYLAND=0
+fi
 
